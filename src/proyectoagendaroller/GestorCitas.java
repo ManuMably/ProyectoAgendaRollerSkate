330f65bb4f9ed38d6d1e2ca5675581be70fb54e3
ManuMably <manupiro96@gmail.com>
2023-12-02 03:20:32 -0500

    listo agendar citas formulario

--- src/proyectoagendaroller/GestorCitas.java
+++ src/proyectoagendaroller/GestorCitas.java
@@ -90,7 +90,7 @@ public class GestorCitas {
                 citasRegistradas[numeroCitasRegistradas].setIdCita(Integer.parseInt(rs.getString("IdCita")));
                 citasRegistradas[numeroCitasRegistradas].setCedulaInstructor(Integer.parseInt(rs.getString("CedulaInstructor")));
                 citasRegistradas[numeroCitasRegistradas].setCedulaAlumno(Integer.parseInt(rs.getString("CedulaAlumno")));
-                citasRegistradas[numeroCitasRegistradas].setFechaCita(rs.getDate("FechaCita").toLocalDate());
+                citasRegistradas[numeroCitasRegistradas].setFechaCita(rs.getDate("FechaCita"));
                 citasRegistradas[numeroCitasRegistradas].setHoraCita(rs.getInt("HoraCita"));   
                 citasRegistradas[numeroCitasRegistradas].setLugarCita(rs.getString("LugarCita"));                
                 citasRegistradas[numeroCitasRegistradas].setNivel(rs.getString("Nivel"));
@@ -107,7 +107,19 @@ public class GestorCitas {
         return -1;
     }
     
-    public void guardarUsuariosRegistrados() {
+    public void guardarCitasRegistradas(Cita citaNueva) {
+        int nuevoTamano = numeroCitasRegistradas + 1;
+        Cita[] nuevoArreglo = new Cita[nuevoTamano];
+
+        // Asignar el nuevo elemento en la primera posición del nuevo arreglo
+        nuevoArreglo[0] = citaNueva;
+
+        // Copiar los elementos del arreglo original al nuevo arreglo, empezando desde la posición 1
+        System.arraycopy(citasRegistradas, 0, nuevoArreglo, 1, numeroCitasRegistradas);
+
+        citasRegistradas = nuevoArreglo;
+        numeroCitasRegistradas = nuevoTamano;
+        
     Connection con = null;
     PreparedStatement ps = null;
 
@@ -134,10 +146,8 @@ public class GestorCitas {
                 ps.setInt(1, cita.getIdCita());
                 ps.setInt(2, cita.getCedulaInstructor());
                 ps.setInt(3,cita.getCedulaAlumno());
-                LocalDate fechaCita = cita.getFechaCita();
-                long millis = fechaCita.atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli();
-                java.sql.Date fechaSql = new java.sql.Date(millis);
-                ps.setDate(4, fechaSql);
+                Date fechaCita = cita.getFechaCita();
+                ps.setDate(4, java.sql.Date.valueOf(fechaCita.toString()));
                 ps.setInt(5, cita.getHoraCita());
                 ps.setString(6, cita.getLugarCita());
                 ps.setString(7, cita.getNivel());
@@ -170,23 +180,7 @@ public class GestorCitas {
             e.printStackTrace();
         }
     }
-}
-    public static int registrarCita(Cita citaNueva){
-        int nuevoTamano = numeroCitasRegistradas + 1;
-        Cita[] nuevoArreglo = new Cita[nuevoTamano];
-
-        // Asignar el nuevo elemento en la primera posición del nuevo arreglo
-        nuevoArreglo[0] = citaNueva;
-
-        // Copiar los elementos del arreglo original al nuevo arreglo, empezando desde la posición 1
-        System.arraycopy(citasRegistradas, 0, nuevoArreglo, 1, numeroCitasRegistradas);
-
-        citasRegistradas = nuevoArreglo;
-        numeroCitasRegistradas = nuevoTamano;
-        return 1;
-    
-    }
-    
+}  
     
     public static Cita buscarCita(int cedula, int idCita){
         
@@ -288,10 +282,8 @@ public class GestorCitas {
                 psUpdate.setInt(1, modificadaCita.getIdCita());
                 psUpdate.setInt(2, modificadaCita.getCedulaInstructor());
                 psUpdate.setInt(3,modificadaCita.getCedulaAlumno());
-                LocalDate fechaCita = modificadaCita.getFechaCita();
-                long millis = fechaCita.atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli();
-                java.sql.Date fechaSql = new java.sql.Date(millis);
-                psUpdate.setDate(4, fechaSql);
+                Date fechaCita = modificadaCita.getFechaCita();
+                psUpdate.setDate(4, java.sql.Date.valueOf(fechaCita.toString()));
                 psUpdate.setInt(5, modificadaCita.getHoraCita());
                 psUpdate.setString(6, modificadaCita.getLugarCita());
                 psUpdate.setString(7, modificadaCita.getNivel());
