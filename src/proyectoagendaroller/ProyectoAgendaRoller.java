a39625cdc5eaf3e45097d32dc5126adec03e83ca
ManuMably <manupiro96@gmail.com>
2023-12-02 04:24:05 -0500

    se conecta el menu con agendar citas

--- src/proyectoagendaroller/ProyectoAgendaRoller.java
+++ src/proyectoagendaroller/ProyectoAgendaRoller.java
@@ -2,30 +2,22 @@
 package proyectoagendaroller;
 
 import java.util.Scanner;
+import javax.swing.SwingUtilities;
+import Interfaces.*;
 
 public class ProyectoAgendaRoller {
 
     
-    public static void main(String[] args) {
-        
-        Scanner leer = new Scanner(System.in);
-        
-        Perfiles perfilActual = new Perfiles();
-        
-        int respuesta = perfilActual.cargarDatosRegistrados();
-        
-        Administrador admin = new Administrador("Royer Pineda", 103666576, 5787836, 3, "ad45", "0000", "rosita");
-        Perfiles.registrarUsuario(admin);
-        
-        Perfiles.actualizarUsuariosRegistrados();
-        
-        perfilActual.guardarUsuariosRegistrados();
-        
-        System.out.println(Perfiles.mostrarDatosUsuariosRegistrados());
-        
+    public static void main(String[] args) {  
+        SwingUtilities.invokeLater(() -> {
+            mostrarVentanaInicial();
+        });
         
         
         
     }
-    
+    private static void mostrarVentanaInicial() {
+         Inicio_Sesion ventanaInicial = new Inicio_Sesion();
+        ventanaInicial.setVisible(true);
+    }
 }
