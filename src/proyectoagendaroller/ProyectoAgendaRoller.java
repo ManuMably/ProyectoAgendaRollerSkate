1fbbc679c9c86d4cb036aefec9f812160f7c8f07
zTMike <zTMike@DESKTOP-8G55D9B>
2023-12-02 10:49:57 -0500

    Ventana agendar citas

--- src/proyectoagendaroller/ProyectoAgendaRoller.java
+++ src/proyectoagendaroller/ProyectoAgendaRoller.java
@@ -1,31 +1,22 @@
 
 package proyectoagendaroller;
 
+import Interfaces.GestionarUsuarios;
+import Interfaces.Inicio_Sesion;
+import Interfaces.Menu;
 import java.util.Scanner;
+import javax.swing.JFrame;
 
 public class ProyectoAgendaRoller {
 
     
     public static void main(String[] args) {
         
-        Scanner leer = new Scanner(System.in);
-        
-        Perfiles perfilActual = new Perfiles();
-        
-        int respuesta = perfilActual.cargarDatosRegistrados();
-        
-        Administrador admin = new Administrador("Royer Pineda", 103666576, 5787836, 3, "ad45", "0000", "rosita");
-        Perfiles.registrarUsuario(admin);
-        
-        Perfiles.actualizarUsuariosRegistrados();
-        
-        perfilActual.guardarUsuariosRegistrados();
-        
-        System.out.println(Perfiles.mostrarDatosUsuariosRegistrados());
-        
-        
-        
+       
+       JFrame menu = new Inicio_Sesion();
+       menu.setVisible(true);
         
     }
+    
     
 }
